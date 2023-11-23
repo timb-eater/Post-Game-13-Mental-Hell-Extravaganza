4007f52318002fae1441668baf86a5398a03982c
timb eater <110617297+timb-eater@users.noreply.github.com>
2023-11-23 18:44:43 -0500

    going to be changing a bunch of shit heres a emergency commit

--- Scenes/components/Dialogue.gd
+++ Scenes/components/Dialogue.gd
@@ -4,11 +4,4 @@ extends Node
 @export var npc: CharacterBody3D
 @export var message : String
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
